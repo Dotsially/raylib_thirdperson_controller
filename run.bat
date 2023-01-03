074b41faed06742d44405afbf693a5c3cfa53665
dots <mateu9999@gmail.com>
2023-01-03 14:40:22 +0100

    fixed run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-gcc -g src/*.c -o TP.exe -O1 -Wall -std=c99 -Wno-missing-braces -I include/ -I headers/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -luser32 -lshell32
+gcc -O3 src/*.c -o TP.exe -O1 -Wall -std=c99 -Wno-missing-braces -I include/ -I headers/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -luser32 -lshell32
